b2fcb210b72083615a5f1d65d9d93569d95abcef
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-04-10 23:24:53 -0300

    Remove - Console.log

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,8 +1,6 @@
 const mongoose = require( 'mongoose' )
 require( 'dotenv/config' )
 
-console.log('databas',  process.env.DATABASE_URL )
-
 mongoose.connect( process.env.DATABASE_URL ,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 mongoose.Promise = global.Promise
 
